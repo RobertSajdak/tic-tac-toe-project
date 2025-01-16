133f9ee4c5d710fdf5a3da51f487eba0e890e8ac
RobertSajdak <robert.sajdak@onet.eu>
2025-01-16 19:14:04 +0100

    added interactivity to the player name field and Edit button

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,12 +1,27 @@
+import { useState } from 'react';
+
 // Komponent gracza.
 export default function Player({ name, symbol }) {
+	const [isEditing, setIsEditing] = useState(false);
+
+	// Funkcja obsługi kliknięcia.
+	function handleEditClick() {
+		setIsEditing(true);
+	}
+
+	let playerName = <span className="player-name">{name}</span>;
+
+	if (isEditing) {
+		playerName = <input type="text" required />;
+	}
+
 	return (
 		<li>
 			<span className="player">
-				<span className="player-name">{name}</span>
+				{playerName}
 				<span className="player-symbol">{symbol}</span>
 			</span>
-			<button>Edit</button>
+			<button onClick={handleEditClick}>Edit</button>
 		</li>
 	);
 }
