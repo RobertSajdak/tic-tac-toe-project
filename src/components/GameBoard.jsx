5514aa8b4005c37075001cf554ad15523c5417c9
RobertSajdak <robert.sajdak@onet.eu>
2025-01-17 17:30:14 +0100

    added implementation of dynamically updating GameBoard component after pressing the button

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const initialGameBoard = [
 	[null, null, null],
 	[null, null, null],
@@ -6,14 +8,27 @@ const initialGameBoard = [
 
 // Komponent z dynamicznym wyświetlaniem siatki pól do gry.
 export default function GameBoard() {
+	// Celem hook'a "useState" tutaj jest zarządzanie stanem i aktualizowanie stanu planszy gry.
+	const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+	function handleSelectSquare(rowIndex, colIndex) {
+		setGameBoard(prevGameBoard => {
+			const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+			updatedBoard[rowIndex][colIndex] = 'X';
+			return updatedBoard;
+		});
+	}
+
 	return (
 		<ol id="game-board">
-			{initialGameBoard.map((row, rowIndex) => (
+			{gameBoard.map((row, rowIndex) => (
 				<li key={rowIndex}>
 					<ol>
 						{row.map((playerSymbol, colIndex) => (
 							<li key={colIndex}>
-								<button>{playerSymbol}</button>
+								<button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+									{playerSymbol}
+								</button>
 							</li>
 						))}
 					</ol>
