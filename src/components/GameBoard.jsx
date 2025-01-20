49e27ebffda6b268e8394e9fbcb1709437948d3d
RobertSajdak <robert.sajdak@onet.eu>
2025-01-20 20:22:25 +0100

    changed implementation of GameBoard component

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,5 +1,3 @@
-import { useState } from 'react';
-
 const initialGameBoard = [
 	[null, null, null],
 	[null, null, null],
@@ -7,19 +5,28 @@ const initialGameBoard = [
 ];
 
 // Komponent z dynamicznym wyświetlaniem siatki pól do gry.
-export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
-	// Celem hook'a "useState" tutaj jest zarządzanie stanem i aktualizowanie stanu planszy gry.
-	const [gameBoard, setGameBoard] = useState(initialGameBoard);
+export default function GameBoard({ onSelectSquare, turns }) {
+	let gameBoard = initialGameBoard;
 
-	function handleSelectSquare(rowIndex, colIndex) {
-		setGameBoard(prevGameBoard => {
-			const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-			updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-			return updatedBoard;
-		});
+	for (const turn of turns) {
+		const { square, player } = turn;
+		const { row, col } = square;
 
-		onSelectSquare();
+		gameBoard[row][col] = player;
 	}
+
+	// Celem hook'a "useState" tutaj jest zarządzanie stanem i aktualizowanie stanu planszy gry.
+	// const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+	// function handleSelectSquare(rowIndex, colIndex) {
+	// 	setGameBoard(prevGameBoard => {
+	// 		const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+	// 		updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+	// 		return updatedBoard;
+	// 	});
+
+	// 	onSelectSquare();
+	// }
 
 	return (
 		<ol id="game-board">
@@ -28,7 +35,7 @@ export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
 					<ol>
 						{row.map((playerSymbol, colIndex) => (
 							<li key={colIndex}>
-								<button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+								<button onClick={() => onSelectSquare(rowIndex, colIndex)}>
 									{playerSymbol}
 								</button>
 							</li>
