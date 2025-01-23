68a77abe36c4c42d7ac25e676789b593628beb5c
RobertSajdak <robert.sajdak@onet.eu>
2025-01-23 15:40:12 +0100

    added implementation indicating the winner of the game

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,36 +1,7 @@
-const initialGameBoard = [
-	[null, null, null],
-	[null, null, null],
-	[null, null, null],
-];
-
-// Komponent z dynamicznym wyświetlaniem siatki pól do gry.
-export default function GameBoard({ onSelectSquare, turns }) {
-	let gameBoard = initialGameBoard;
-
-	for (const turn of turns) {
-		const { square, player } = turn;
-		const { row, col } = square;
-
-		gameBoard[row][col] = player;
-	}
-
-	// Celem hook'a "useState" tutaj jest zarządzanie stanem i aktualizowanie stanu planszy gry.
-	// const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-	// function handleSelectSquare(rowIndex, colIndex) {
-	// 	setGameBoard(prevGameBoard => {
-	// 		const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-	// 		updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-	// 		return updatedBoard;
-	// 	});
-
-	// 	onSelectSquare();
-	// }
-
+export default function GameBoard({ onSelectSquare, board }) {
 	return (
 		<ol id="game-board">
-			{gameBoard.map((row, rowIndex) => (
+			{board.map((row, rowIndex) => (
 				<li key={rowIndex}>
 					<ol>
 						{row.map((playerSymbol, colIndex) => (
